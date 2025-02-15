dd60664f76eb0eb5083fcc64ce3d8a77b1a80619
Justyna W <jkwodecka@gmail.com>
2025-02-15 14:04:59 +0100

    improve theme context

--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -1,16 +1,14 @@
+import { useContext } from 'react';
 import { ThemeContext } from './ThemeContext'
 import {Header} from './Header'
 import {UserSearch} from './UserSearch'
 
 export const MainApp = () => {
+    const {theme} = useContext(ThemeContext)
     return (
-        <ThemeContext.Consumer>
-            {({theme}) => (
                 <div className={`main ${theme}`}>
                     <Header/>
                     <UserSearch/>
                 </div>
-            )}
-        </ThemeContext.Consumer>
     )
 }
